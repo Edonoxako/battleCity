54b27fe4094d217cd062a824a54261deb0cf0637
Aoned <a1d@bk.ru>
2015-11-12 05:08:58 +0700

    Changed the class, added comments

--- src/core/managers/ProcessManager.java
+++ src/core/managers/ProcessManager.java
@@ -1,62 +1,92 @@
 package core.managers;
 
 import java.util.ArrayList;
-import core.model.Process;
 
-public class ProcessManager extends Thread{
-private ArrayList<Process> ProcessList;
+import core.model.Process;
+import core.model.Process.ProcessState;
+
+public class ProcessManager{
+private ArrayList<Process> ProcList;
 	
 	public ProcessManager() {
-		ProcessList = new ArrayList<Process>(5);
+		ProcList = new ArrayList<Process>(5);
 	}
 	
 	public ArrayList<Process> getProcList(){
-		return ProcessList;
+		return ProcList;
 	}
-	
-	public void addProc(Process p) {
-		if(!ProcessList.contains(p)){
-			ProcessList.add(p);
+	//Добавляет процесс в список, возвращая true. Если в списке есть такой процесс вернет false.
+	public boolean addProc(Process p) {
+		if(!ProcList.contains(p)){
+			ProcList.add(p);
+			return true;
 		}else{
-			System.out.println("This element is already in the list");
+			return false;
 		}
 	}
 	
-	public void removeProc(Process p) {
-		if(ProcessList.remove(p)){
-		}
-		
+	public boolean removeProc(Process p) {
+		return ProcList.remove(p);	
 	}
-	
+	//Возвращает индекс процесса в списке, если такого процесса нет вернет -1;
 	public int getIndexOf(Process p){
-		return ProcessList.indexOf(p);
+		return ProcList.indexOf(p);
 	}
-	
-	public void startAll() {
-		if(ProcessList.isEmpty()){
-			System.out.println("ProcessLIst is empty");
-			return;
-		}
-		for(Process p: ProcessList){
+	//Запустит все процессы из списка, вернув true. Если список пуст вернет false.
+	public boolean startAll() {
+		if(ProcList.isEmpty())
+			return false;
+		
+		for(Process p: ProcList){
 			p.start();
 		}
+		return true;
 	}
 	
-	public void killAll() {
-		for(Process p: ProcessList){
+	//Уничтожает все процессы из списка вернув true. Вернет false если список был пуст.
+	public boolean killAll() {
+		if(ProcList.isEmpty())
+			return false;
+		for(Process p: ProcList){
 			p.stop();
 		}
-		ProcessList.removeAll(ProcessList);
+		return ProcList.removeAll(ProcList);
 	}
 	
-	public void stopAll() {
-		for(Process p: ProcessList){
-			try {
-				p.wait();
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+	
+	//Приостанавливает все процессы вернув true. Вернет false если список был пуст.
+	public boolean stopAll() {
+		if(ProcList.isEmpty())
+			return false;
+		
+		for(Process p: ProcList){
+				if(p.getProcessState() != ProcessState.waiting.getState())
+				p.pause();
 		}
+		return true;
+	}
+	
+	//Приостанавливает процесс
+	public void stop(Process p){
+		if(ProcList.contains(p)){
+			if(p.getProcessState() != ProcessState.waiting.getState())
+				p.pause();
+		}
+		return;
+	}
+	//Запускает процесс
+	public void start(Process p){
+		if(ProcList.contains(p)){
+				p.start();
+		}
+		return;
+	}
+	
+	//Уничтожает процесс
+	public void kill(Process p){
+		if(ProcList.contains(p)){
+				p.stop();
+		}
+		return;
 	}
 }
