d51abeb586033585593fdb07002bbc52686ff1fb
Aoned <a1d@bk.ru>
2015-11-11 12:49:42 +0700

    add method

--- src/core/model/Process.java
+++ src/core/model/Process.java
@@ -1,5 +1,30 @@
 package core.model;
 
-public class Process {
-
+public abstract class Process implements Runnable{
+	private Thread thread;
+	protected boolean running;
+	
+	public synchronized void start(){
+			
+			if(running)
+				return;
+			running = true;
+			thread = new Thread(this);
+			thread.start();
+			
+	}
+	
+	public synchronized void stop(){
+		if(!running)
+			return;
+		
+		running = false;
+		
+		try {
+			Thread.sleep(1);
+			thread.interrupt();
+		} catch (InterruptedException e){
+			e.printStackTrace();
+		}
+	}
 }
