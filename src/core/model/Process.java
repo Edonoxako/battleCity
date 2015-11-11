62d5bcee9afa40a1e9b748b8fb7cc89be97cab45
Aoned <a1d@bk.ru>
2015-11-12 05:10:35 +0700

    Changed the model, added comments

--- src/core/model/Process.java
+++ src/core/model/Process.java
@@ -2,29 +2,88 @@ package core.model;
 
 public abstract class Process implements Runnable{
 	private Thread thread;
-	protected boolean running;
-	
-	public synchronized void start(){
-			
-			if(running)
-				return;
-			running = true;
-			thread = new Thread(this);
-			thread.start();
-			
+	public enum ProcessState{
+		init(0),
+		running(2),
+		waiting(3),
+		interrupt(4);
+		
+		private int state;
+		
+		ProcessState(int state){
+			this.state = state;
+		}
+		
+		public int getState(){
+			return state;
+		}
 	}
 	
+	private ProcessState state;
+	public Process(){
+		init();
+	}
+	protected void init(){
+		state = ProcessState.init;
+		thread = new Thread(this);
+	}
+	//Запускает процесс, инициализируя его снова если того требует состояние
+	public synchronized void start(){
+			if( state == ProcessState.interrupt){
+				init();
+				state = ProcessState.running;
+				thread.start();
+			}else if( state == ProcessState.waiting){
+				state = ProcessState.running;
+				thread.notify();
+			}else{
+				return;
+			}
+			
+	}
+	//Останавливает процесс, прерывая его поток
 	public synchronized void stop(){
-		if(!running)
+		if(state!=ProcessState.interrupt || state!=ProcessState.init)
 			return;
 		
-		running = false;
+		state = ProcessState.interrupt;
 		
 		try {
 			Thread.sleep(1);
 			thread.interrupt();
 		} catch (InterruptedException e){
-			e.printStackTrace();
+			return;
+		}
+	}
+	//Приостанавливает процесс
+	public synchronized void pause(){
+		if(state != ProcessState.running)
+			return;
+		
+		state = ProcessState.waiting;
+		
+		try {
+			thread.wait();
+		} catch (InterruptedException e){
+			if( thread.isInterrupted()){
+				return;
+			}
+		}
+	}
+	public int getProcessState(){
+		return state.getState();
+	}
+	//Приостанавливает процесс на заданное время
+	public synchronized void pause(long mils){
+		if(state != ProcessState.running)
+			return;
+		
+		try {
+			thread.wait(mils);
+		} catch (InterruptedException e){
+			if( thread.isInterrupted()){
+				return;
+			}
 		}
 	}
 }
