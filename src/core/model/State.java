2c5a469c7de489fe0d4c71ee50e11845d3207577
Aoned <a1d@bk.ru>
2015-11-15 05:45:36 +0700

    update State model

--- src/core/model/State.java
+++ src/core/model/State.java
@@ -1,7 +1,13 @@
 package core.model;
 
 public abstract class State {
+private boolean isInit;
+	
 	public void init(){
-		
+		isInit = true;
+	}
+	//Возвращает флаг инициализации.
+	public boolean isInit(){
+		return isInit;
 	}
 }
